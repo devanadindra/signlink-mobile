d7929b00c009ab1d3dccd603cc5f36dc07f9dbeb
devanadindraa <devanadindra.p@gmail.com>
2025-10-29 21:15:14 +0700

    style homescreen

--- mobile/app/src/main/java/com/example/signlink/ui/theme/Color.kt
+++ mobile/app/src/main/java/com/example/signlink/ui/theme/Color.kt
@@ -13,7 +13,6 @@ val Pink40 = Color(0xFF7D5260)
 
 val DarkText = Color(0xFF1F1F1F)
 val LightText = Color(0xFF757575)
-val DotActive = Color(0xFF0094AE)
 val CardBackground = Color.White
 val LightGrayBackground = Color(0xFFE0E0E0)
 val LightTealBackground = Color(0xFFE0F7FA)
