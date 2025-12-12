2b76917525f006db6148c84d0e2591452791ff43
devanadindraa <devanadindra.p@gmail.com>
2025-12-12 16:59:05 +0700

    res for kuis

--- back-end/domains/kuis/response.go
+++ back-end/domains/kuis/response.go
@@ -1,29 +1,40 @@
 package kuis
 
-type LatihanRes struct {
+type KuisRes struct {
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	TotalSoal  int    `json:"total_soal"`
+	BatasWaktu int    `json:"batas_waktu"`
+	IsDone     bool   `json:"is_done"`
+}
+
+type KuisByIdRes struct {
+	ID          string       `json:"id"`
+	Name        string       `json:"name"`
+	TotalSoal   int          `json:"total_soal"`
+	BatasWaktu  int          `json:"batas_waktu"`
+	SoalLatihan []SoaKuisRes `json:"soal_kuis"`
+}
+
+type SoaKuisRes struct {
+	ID           string        `json:"id"`
+	ModulID      string        `json:"modul_id"`
+	Soal         string        `json:"soal"`
+	JawabanBenar string        `json:"jawaban_benar"`
+	OpsiKuis     []OpsiKuisRes `json:"opsi_kuis"`
+}
+
+type OpsiKuisRes struct {
+	ID     string `json:"id"`
+	SoalID string `json:"soal_id"`
+	Label  string `json:"label"`
+	Text   string `json:"text"`
+}
+
+type StatsKuisRes struct {
 	ID        string `json:"id"`
-	Name      string `json:"name"`
+	ModulID   string `json:"modul_id"`
+	KuisName  string `json:"kuis_name"`
 	TotalSoal int    `json:"total_soal"`
-	IsDone    bool   `json:"is_done"`
-}
-
-type LatihanByIdRes struct {
-	ID          string           `json:"id"`
-	Name        string           `json:"name"`
-	TotalSoal   int              `json:"total_soal"`
-	SoalLatihan []SoalLatihanRes `json:"soal_latihan"`
-}
-
-type SoalLatihanRes struct {
-	ID        string `json:"id"`
-	LatihanID string `json:"latihan_id"`
-	Soal      string `json:"soal"`
-}
-
-type StatsLatihanRes struct {
-	ID          string  `json:"id"`
-	LatihanID   string  `json:"latihan_id"`
-	LatihanName string  `json:"latihan_name"`
-	TotalSoal   int     `json:"total_soal"`
-	Score       float64 `json:"score"`
+	Score     int    `json:"score"`
 }
